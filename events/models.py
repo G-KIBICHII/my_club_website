b0f09712f5cd8db308c1eb9be8ab2bd6599dd6da
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-06-29 18:04:11 +0300

    setup models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Venue(models.Model):
+    name = models.CharField("Venue name", max_length=120)
+    address =models.CharField(max_length=300)
+    zip_code=models.CharField(max_length=15)
+    phone = models.CharField('Contact Phone',max_length=25)
+    web =models.URLField()
+    email_address =models.EmailField("Email Address")
+
+    def __str__(self):
+        return self.name
+
+
+class MyClubUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email_address =models.EmailField("user Address")
+
+    def __str__(self):
+        return self.first_name +" " + self.last_name
+
+class Event(models.Model):
+    name= models.CharField("Event Name",max_length=120)
+    event_date =models.DateTimeField("Event date")
+    venue =models.ForeignKey(Venue,blank=True,null=True,on_delete=models.CASCADE)
+    manager = models.CharField(max_length=60)
+    description =models.TextField(blank=True)
+    attendees = models.ManyToManyField(MyClubUser, blank=True)
+    def __str__(self):
+        return self.name
