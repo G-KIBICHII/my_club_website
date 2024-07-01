857fa93a517876138640c83dbc07d4794ae5bffb
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-07-02 01:36:25 +0300

    setup admin area

--- events/admin.py
+++ events/admin.py
@@ -4,6 +4,22 @@ from .models import Event
 from .models import MyClubUser
 
 # Register your models here.
-admin.site.register(Venue)
-admin.site.register(Event)
+# admin.site.register(Venue,VenueAdmin)
+# admin.site.register(Event)
 admin.site.register(MyClubUser)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display=('name','address','phone')
+    ordering =('name',)
+    search_fields=('name','address')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields =(('name','venue','event_date','description','manager','attendees'))
+    list_display = ('name','event_date','venue')
+    list_filter =('event_date','venue')
+    ordering =('event_date',)
+
+
+# admin.site.register(Venue,VenueAdmin)
--- events/models.py
+++ events/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Venue(models.Model):
     name = models.CharField("Venue name", max_length=120)
     address =models.CharField(max_length=300)
     zip_code=models.CharField(max_length=15)
-    phone = models.CharField('Contact Phone',max_length=25)
-    web =models.URLField()
-    email_address =models.EmailField("Email Address")
+    phone = models.CharField('Contact Phone',max_length=25,blank=True)
+    web =models.URLField(blank=True)
+    email_address =models.EmailField("Email Address",blank=True)
 
     def __str__(self):
         return self.name
@@ -25,7 +26,8 @@ class Event(models.Model):
     name= models.CharField("Event Name",max_length=120)
     event_date =models.DateTimeField("Event date")
     venue =models.ForeignKey(Venue,blank=True,null=True,on_delete=models.CASCADE)
-    manager = models.CharField(max_length=60)
+    # manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User,blank=True,null=True,on_delete=models.SET_NULL)
     description =models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
     def __str__(self):
