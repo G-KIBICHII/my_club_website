02b56be37b23a880213d1171c675cb45e79d5271
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-07-02 18:40:15 +0300

    additin

--- events/forms.py
+++ events/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Venue
+from .models import Venue,Event
 
 class VenueForm(ModelForm):
     class Meta:
@@ -23,4 +23,26 @@ class VenueForm(ModelForm):
             'web':forms.TextInput(attrs={'class':'form-control',"placeholder":"web adress"}),
             'email_address':forms.EmailInput(attrs={'class':'form-control',"placeholder":"Email"})
               }
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields =('name','event_date','venue','manager','attendees','description')
+        labels={
+            'name':'',
+            'event_date':'yyyy-MM-DD HH:MM:SS',
+            'venue':'Venue ',
+            'manager':'manager ',
+            'description':'',
+            'attendees':'atendeees'
+        }
+
+        widgets ={
+            'name': forms.TextInput(attrs={'class':'form-control',"placeholder":"Event Name"}),
+            'event_date':forms.TextInput(attrs={'class':'form-control',"placeholder":"Event date"}),
+            'venue':forms.Select(attrs={'class':'form-select',"placeholder":"venue"}),
+            'manager':forms.Select(attrs={'class':'form-select',"placeholder":"manager"}),
+            'description':forms.Textarea(attrs={'class':'form-control',"placeholder":"description"}),
+            'attendees':forms.SelectMultiple(attrs={'class':'form-control',"placeholder":"Atendees"})
+              }
         
+
