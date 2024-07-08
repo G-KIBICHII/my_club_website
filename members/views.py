03b977ac304a483387e1d91b83c95747ee346ee8
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-07-08 23:43:01 +0300

    add a register user

--- members/views.py
+++ members/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 # Create your views here.
+
+
 
 def login_user(request):
     if request.method=="POST":
@@ -23,6 +27,23 @@ def logout_user(request):
     logout(request)
     messages.success(request,("you were loged out"))
     return redirect('home')
+
+
+def register_user(request):
+    form = RegisterUserForm()
+    if request.method =="POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,("registration successfull"))
+            return redirect('home')
+        else:
+            form = RegisterUserForm()
+    return render(request,'authenticate/register_user.html',{'form':form})
 
     
 
